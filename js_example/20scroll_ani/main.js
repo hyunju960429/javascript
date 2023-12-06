3e3fd794aa672e79d9869276ce333b2843585a68
hyunju960429 <145514544+hyunju960429@users.noreply.github.com>
2023-12-06 17:16:41 +0900

    Add files via upload

---
+++ js_example/20scroll_ani/main.js
@@ -0,0 +1,24 @@
+let stageEle = document.querySelector('.stage'),
+    houseEle = document.querySelector('.house'),
+    maxScrollValue, //실제 스크롤이 움직이는 높이값
+    mousePosition = {x:0,y:0};
+
+    function resizeHandler(){
+        maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    }
+    window.addEventListener('scroll',()=>{
+        let scrollPer = pageYOffset / maxScrollValue;
+        //console.log(scrollPer)
+
+        let zMove = scrollPer * 1200 - 490;
+        houseEle.style.transform = `translateZ(${zMove}vw)`;
+    })
+
+    window.addEventListener('mousemove',(e)=>{
+        mousePosition.x = (e.clientX / window.innerWidth) * 5;
+        mousePosition.y = -(e.clientY / window.innerHeight) * 5;
+        stageEle.style.transform = `rotateY(${mousePosition.x}deg) rotateX(${mousePosition.y}deg)`;
+    })
+
+    window.addEventListener('resize',resizeHandler)
+    resizeHandler();
